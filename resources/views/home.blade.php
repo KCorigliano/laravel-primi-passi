2371f90b740d87d65f906b6db2053e4e67187b32
KCorigliano <corigliano98@hotmail.com>
2022-03-02 15:51:51 +0100

    navbar + style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-    <h1>Hello World!</h1>
+<body class="bg-secondary">
+    <header>
+        <div class="d-flex justify-content-center py-2 bg-primary">
+            <h3 class="mx-4"><a class="text-light" href="contacts">Contacts</a></h3>
+            <h3 class="mx-4"><a class="text-light" href="about-us">About us</a></h3>
+            <h3 class="mx-4"><a class="text-light" href="info">Info</a></h3>
+            <h3 class="mx-4"><a class="text-light" href="team">Team</a></h3>
+        </div>
+    </header>
+    <div class="text-center py-4">
+        <h1>Hello World!</h1>
+    </div>
 </body>
 </html>
